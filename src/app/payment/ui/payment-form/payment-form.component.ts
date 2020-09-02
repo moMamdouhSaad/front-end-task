3b442e6638215e9d519ef8171c8debd25d9028ae
mohamed mamdouh <moMamdouhSaad@gmail.com>
2020-09-02 08:49:16 +0200

    added payment formgroup

--- src/app/payment/ui/payment-form/payment-form.component.ts
+++ src/app/payment/ui/payment-form/payment-form.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  EventEmitter,
+  Output,
+} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'payment-form',
@@ -7,7 +14,81 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PaymentFormComponent implements OnInit {
-  public constructor() {}
+  public constructor(private readonly fb: FormBuilder) {}
+  public paymentFormGroup: FormGroup;
+  public dateErrFlag = false;
 
-  public ngOnInit(): void {}
+  @Output() public readonly submitPayment: EventEmitter<
+    FormGroup
+  > = new EventEmitter<FormGroup>();
+
+  public ngOnInit(): void {
+    this.initPaymentForm();
+  }
+
+  private initPaymentForm(): void {
+    this.paymentFormGroup = this.fb.group({
+      cardholder: ['', [Validators.required]],
+      cardno: ['', [Validators.required]],
+      cvc: [
+        '',
+        [
+          Validators.maxLength(3),
+          Validators.minLength(3),
+          Validators.pattern(/^[0-9]\d*$/),
+        ],
+      ],
+      month: ['', [Validators.required]],
+      year: [
+        '',
+        [Validators.required, Validators.maxLength(4), Validators.minLength(4)],
+      ],
+      amount: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^[0-9]\d*$/),
+          Validators.min(1),
+        ],
+      ],
+    });
+  }
+
+  public submit(): void {
+    console.log(this.paymentFormGroup.value);
+  }
+
+  public numberValidation(event): boolean {
+    const key = event.key ? event.keyCode : event.which;
+    if (event.keyCode === 8 || event.keyCode === 46) {
+      return true;
+    } else if (key < 48 || key > 57) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  public dateValidation(): void {
+    const monthInput = parseInt(this.paymentFormGroup.get('month').value, 10);
+    const yearInput = parseInt(this.paymentFormGroup.get('year').value, 10);
+    const currentDate = new Date().toISOString().split('T')[0].split('-');
+    const currentYear = parseInt(currentDate[0], 10);
+    const currentMonth = parseInt(currentDate[1], 10);
+
+    if (
+      !this.paymentFormGroup.controls.year.dirty ||
+      !this.paymentFormGroup.controls.month.dirty
+    ) {
+      return;
+    }
+    if (
+      (yearInput === currentYear && monthInput >= currentMonth) ||
+      yearInput > currentYear
+    ) {
+      this.dateErrFlag = false;
+    } else {
+      this.dateErrFlag = true;
+    }
+  }
 }
--- src/app/payment/ui/ui.module.ts
+++ src/app/payment/ui/ui.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaymentFormComponent } from './payment-form/payment-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [PaymentFormComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: [PaymentFormComponent],
 })
 export class UiModule {}
